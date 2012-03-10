842eb8396b019dff13c41bb5534b1a83e84d3881
Ben Poweski <bpoweski@gmail.com>
2012-03-10 17:30:07 -0600

    add documentation to ObjectTypecaster for #77

--- lib/active_attr/typecasting/object_typecaster.rb
+++ lib/active_attr/typecasting/object_typecaster.rb
@@ -1,6 +1,22 @@
 module ActiveAttr
   module Typecasting
+    # A typecaster that does nothing as anything passed is already an object.
+    #
+    # @example Usage
+    #   ObjectTypecaster.new.call("") #=> ""
+    #
+    # @since 0.5.0
     class ObjectTypecaster
+      # Typecasts an object to an object
+      #
+      # @example Typecast an Object
+      #   typecaster.call(1)  #=> 1
+      #
+      # @param [Object] value The object to typecast
+      #
+      # @return [Object] Whatever you passed in
+      #
+      # @since 0.5.0
       def call(value)
         value
       end
