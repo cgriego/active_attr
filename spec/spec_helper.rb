60c5e6be275083c78402e20df04d90e04e9f6cd5
Chris Griego <cgriego@gmail.com>
2012-03-16 01:21:24 -0500

    Filter specs based on ActiveModel version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require "bundler/setup"
 require "rspec/autorun"
-require "active_support"
-require "active_model/mass_assignment_security"
+require "active_model/version"
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
@@ -11,18 +10,7 @@ RSpec.configure do |config|
   config.mock_framework = :rspec
   config.treat_symbols_as_metadata_keys_with_true_values = true # default in RSpec 3
 
-  ActiveModelCheck = Class.new do
-    include ActiveModel::MassAssignmentSecurity
-    def old_rails?
-      method(:sanitize_for_mass_assignment).arity.abs == 1
-    end
-  end
-
-  if ActiveModelCheck.new.old_rails?
-    puts "old rails detected"
-    config.filter_run_excluding :roles_support => true
-  else
-    puts "new rails detected"
-    config.filter_run_excluding :without_roles_support => true
-  end
+  config.filter_run_excluding :active_model_version => lambda { |requirement|
+    !Gem::Requirement.create(requirement).satisfied_by?(Gem::Version.new(ActiveModel::VERSION::STRING))
+  }
 end
--- spec/unit/active_attr/mass_assignment_security_spec.rb
+++ spec/unit/active_attr/mass_assignment_security_spec.rb
@@ -15,14 +15,19 @@ module ActiveAttr
     shared_examples "secure mass assignment method", :secure_mass_assignment_method => true do
       include_examples "mass assignment method"
 
-      it "ignores assigning a protected attribute" do
+      it "ignores assigning an attribute protected by role-based security", :active_model_version => ">= 3.1.0" do
         person = mass_assign_attributes(:age => 21)
+        person.age.should be_nil
+      end
+
+      it "ignores assigning a protected attribute" do
+        person = mass_assign_attributes(:first_name => "Chris")
         person.age.should be_nil
       end
     end
 
     shared_examples "secure mass assignment method with options", :secure_mass_assignment_method_with_options => true do
-      it "supports role-based mass assignment security", :roles_support => true do
+      it "supports role-based mass assignment security", :active_model_version => ">= 3.1.0" do
         person = mass_assign_attributes_with_options({ :age => 21 }, :as => :admin)
         person.age.should == 21
       end
@@ -37,52 +42,26 @@ module ActiveAttr
       before do
         model_class.class_eval do
           attr_accessible :first_name, :last_name, :name
+          attr_accessible :age, :as => :admin
         end
       end
 
-      context "with roles support", :roles_support => true do
-        before do
-          model_class.class_eval do
-            attr_accessible :age, :as => :admin
-          end
-        end
-
-        describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-        describe "#attributes=", :attributes=, :secure_mass_assignment_method
-        describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-      end
-
-      context "without roles support", :without_roles_support => true do
-        describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-        describe "#attributes=", :attributes=, :secure_mass_assignment_method
-        describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-      end
+      describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
+      describe "#attributes=", :attributes=, :secure_mass_assignment_method
+      describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
     end
 
     context "black listing attributes" do
       before do
         model_class.class_eval do
           attr_protected :age
+          attr_protected :as => :admin
         end
       end
 
-      context "with roles support", :roles_support => true do
-        before do
-          model_class.class_eval do
-            attr_protected :as => :admin
-          end
-
-          describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-          describe "#attributes=", :attributes=, :secure_mass_assignment_method
-          describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-        end
-      end
-
-      context "without roles support", :without_roles_support => true do
-        describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-        describe "#attributes=", :attributes=, :secure_mass_assignment_method
-        describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
-      end
+      describe "#assign_attributes", :assign_attributes, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
+      describe "#attributes=", :attributes=, :secure_mass_assignment_method
+      describe "#initialize", :initialize, :secure_mass_assignment_method, :secure_mass_assignment_method_with_options
     end
   end
 end
