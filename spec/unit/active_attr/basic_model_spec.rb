284eb3211b436d545ddb8368fdf092d697c31bfe
Micah Frost <micah.frost@gmail.com>
2012-03-27 12:53:07 -0400

    Add basic test for default value of persisted?

--- spec/unit/active_attr/basic_model_spec.rb
+++ spec/unit/active_attr/basic_model_spec.rb
@@ -15,5 +15,11 @@ module ActiveAttr
 
     subject { model_class.new }
     it_should_behave_like "ActiveModel"
+
+    describe "#persisted?" do
+      it "is not persisted by default" do
+        subject.should_not be_persisted
+      end
+    end
   end
 end
