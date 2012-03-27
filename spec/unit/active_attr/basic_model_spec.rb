7514d440efd712dfde01fb80b410b908fc4cd3a7
Micah Frost <micah.frost@gmail.com>
2012-03-27 13:55:40 -0400

    Syntactic sugar for delcaring when a model should be considered persisted

--- spec/unit/active_attr/basic_model_spec.rb
+++ spec/unit/active_attr/basic_model_spec.rb
@@ -21,5 +21,47 @@ module ActiveAttr
         subject.should_not be_persisted
       end
     end
+
+    describe ".persisted" do
+      let :persisted_class do
+        Class.new(model_class) do
+          always_persisted
+        end
+      end
+
+      let :unpersisted_class do
+        Class.new(model_class) do
+          never_persisted
+        end
+      end
+
+      let :lambda_class do
+        Class.new(model_class) do
+          persisted_when { |model| model.to_be_persisted? }
+        end
+      end
+
+      describe "when persistence is true" do
+        subject { persisted_class.new }
+        it { should be_persisted }
+      end
+
+      describe "when persisted is false" do
+        subject { unpersisted_class.new }
+        it { should_not be_persisted }
+      end
+
+      describe "when persisted is a lambda" do
+        subject { lambda_class.new }
+
+        it "evaluates the lambda to determine persistence" do
+          subject.stub(:to_be_persisted?).and_return(false)
+          subject.should_not be_persisted
+
+          subject.stub(:to_be_persisted?).and_return(true)
+          subject.should be_persisted
+        end
+      end
+    end
   end
 end
