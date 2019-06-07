4ff51fad36dc70242e5f75698435e233d7fedf56
Chris Griego <cgriego@gmail.com>
2019-06-07 14:52:34 -0500

    Referencing DataMapper in the docs dates us. [ci skip]

--- lib/active_attr.rb
+++ lib/active_attr.rb
@@ -4,7 +4,7 @@ require "active_support/dependencies/autoload"
 # ActiveAttr is a set of modules to enhance Plain Old Ruby Objects (POROs)
 #
 # These modules give your objects the type of features that are normally found
-# in popular Object Relation Mappers (ORMs) like ActiveRecord, DataMapper, and
+# in popular Object Relation Mappers (ORMs) like ActiveRecord, Sequel, and
 # Mongoid. The goal is to lower the bar for creating easy-to-use Ruby models.
 module ActiveAttr
   extend ActiveSupport::Autoload
